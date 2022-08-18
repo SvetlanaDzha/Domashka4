c6d47bbe9c1f0c52f24e3c3ad9c758905d93259b
Svetlana <svetlanka2503@mail.ru>
2022-08-18 23:44:25 +0300

    Task029

--- Task029/Program.cs
+++ Task029/Program.cs
@@ -1,3 +1,19 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
-// Вводим массив через консоль
 
+
+Console.Write("Введите 8 чисел: ");
+
+string str = Console.ReadLine();
+char[] arr = new char[str.Length]; 
+if (str.Length != 8) 
+{
+Console.WriteLine($"Нужно вводить 8 чисeл");
+}
+else   
+       { 
+        for (int i = 0; i < str.Length; i++)
+         { 
+            arr[i] = str[i]; 
+         } 
+        Console.WriteLine(string.Join(", ", arr)); 
+    }
